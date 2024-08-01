d391c72a3905209cbe19eff7337c2f2b48351c6a
hanakhin <hanakhin.nounimassotte@gmail.com>
2024-08-01 16:45:31 +0200

    fixeed

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -18,7 +18,7 @@ const projectsData: Project[] = [
 
 const ProjectsSection: React.FC = () => {
     return (
-        <section id="projects" className="py-20">
+        <section id="projects" className="py-20 h-screen ">
         <div className="container mx-auto">
             <h2 className="text-3xl font-bold mb-8 text-center text-black">Mes Projets</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
