85a4641b230a4cd6039cd7665eb2ed737996f199
hanakhin <hanakhin.nounimassotte@gmail.com>
2024-08-02 09:53:26 +0200

    fixed grid

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -42,7 +42,7 @@ const SkillIcon: React.FC<{ skill: Skill }> = ({ skill }) => (
 );
 
 const SkillGrid: React.FC<{ skills: Skill[] }> = ({ skills }) => (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
+    <div className="grid-skills grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
         {skills.map((skill, index) => (
             <SkillIcon key={index} skill={skill} />
         ))}
