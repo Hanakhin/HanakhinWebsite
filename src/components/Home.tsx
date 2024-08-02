ba310c590e2d2be7b66b1942d4e376952d73d43f
hanakhin <hanakhin.nounimassotte@gmail.com>
2024-08-02 10:20:20 +0200

    added things

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -79,7 +79,7 @@ const Home: React.FC<HomeProps> = ({ onPageChange }) => {
             <div className="absolute inset-0 bg-gradient-to-r from-blue-300 via-purple-300 to-pink-300 opacity-30 blur-lg"></div>
             <div ref={mountRef} className="absolute inset-0"></div>
             <div className="relative z-10 text-center p-10 bg-white bg-opacity-20 backdrop-blur-lg rounded-xl shadow-lg">
-                <h1 className="text-4xl font-bold mb-4 text-black">Bonjour, je suis Hanakhin</h1>
+                <h1 className="text-4xl font-bold mb-4 text-black ">Bonjour, je suis Hanakhin</h1>
                 <p className="text-lg mb-8 text-black">Bienvenue sur mon site web !</p>
                 <div className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-4">
                     <button
